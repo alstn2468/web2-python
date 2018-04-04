bfbb75fe8becf87f7ce675b13e99166c63c31b3e
alstn2468 <alstn2468_@naver.com>
2018-04-04 22:21:12 +0900

    2018-04-04

--- index.py
+++ index.py
@@ -1,14 +1,20 @@
 #!python
 print("Content-Type: text/html")
 print()
-import cgi
+import cgi, os
+
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
-    description = open('data/' + pageId + '.txt', 'r').read()
+    description = open('data/'+pageId, 'r').read()
 else:
     pageId = 'Welcome'
-    description = 'Hello web'
+    description = 'Hello, web'
 print('''<!doctype html>
 <html>
 <head>
@@ -18,12 +24,10 @@ print('''<!doctype html>
 <body>
   <h1><a href="index.py">WEB</a></h1>
   <ol>
-    <li><a href="index.py?id=HTML">HTML</a></li>
-    <li><a href="index.py?id=CSS">CSS</a></li>
-    <li><a href="index.py?id=JavaScript">JavaScript</a></li>
+    {listStr}
   </ol>
   <h2>{title}</h2>
   <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId, desc = description))
+'''.format(title=pageId, desc=description, listStr=listStr))
